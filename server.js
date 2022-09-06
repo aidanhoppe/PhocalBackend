14735d931139eb8644a2fbd861bb7d61fa049407
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-05 21:04:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const express = require('express')
 const morgan = require('morgan')
-const mysql = require('mysql2')
+const { Pool } = require('pg')
 
 const app = express()
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 
 // https://gist.githubusercontent.com/meech-ward/1723b2df87eae8bb6382828fba649d64/raw/ee52637cc953df669d95bb4ab68ac2ad1a96cd9f/lotr.sql
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
 })
 
-function getRandomInt(max) {
-  return 1 + Math.floor(Math.random() * (max-1))
-}
+// function getRandomInt(max) {
+//   return 1 + Math.floor(Math.random() * (max-1))
+// }
 
-async function getCharacter(id) {
-  const [characters] = await pool.promise().query("SELECT * FROM characters WHERE id = ?", [
-    id,
-  ])
-  return characters[0]
-}
-async function randomId() {
-  const [rows] = await pool.promise().query(
-    "SELECT COUNT(*) as totalCharacters FROM characters"
-  )
-  const { totalCharacters } = rows[0]
-  const randomId = getRandomInt(totalCharacters)
-  return randomId
-}
+// async function getCharacter(id) {
+//   const [characters] = await pool.promise().query("SELECT * FROM characters WHERE id = ?", [
+//     id,
+//   ])
+//   return characters[0]
+// }
+// async function randomId() {
+//   const [rows] = await pool.promise().query(
+//     "SELECT COUNT(*) as totalCharacters FROM characters"
+//   )
+//   const { totalCharacters } = rows[0]
+//   const randomId = getRandomInt(totalCharacters)
+//   return randomId
+// }
 
 app.get("/test", (req, res) => {
   res.send("<h1>It's working 🤗</h1>")
@@ -39,9 +39,9 @@ app.get("/test", (req, res) => {
 
 app.get("/", async (req, res) => {
   try {
-    const id = await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
+    // const id = await randomId()
+    // const character = await getCharacter(id)
+    res.send('success')
   } catch (error) {
     res.send(error)
   }
@@ -49,9 +49,9 @@ app.get("/", async (req, res) => {
 
 app.get("/:id", async (req, res) => {
   try {
-    const id = parseInt(req.params.id) || await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
+    // const id = parseInt(req.params.id) || await randomId()
+    // const character = await getCharacter(id)
+    res.send('id')
   } catch (error) {
     res.send(error)
   }
