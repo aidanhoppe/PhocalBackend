28a352ddb7f536036962322a22543df2993de253
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-08 08:58:12 -0700

    small update

--- s3.js
+++ s3.js
@@ -8,8 +8,10 @@ dotenv.config()
 
 const region = "us-west-1"
 const bucketName = "pdevfree-bucket"
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const accessKeyId = "AKIA3PVOPM4ANEXPHPW2"
+const secretAccessKey = "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"
+// const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 const s3 = new aws.S3({
   region,
